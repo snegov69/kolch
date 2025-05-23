e81b5dfd0659308df98bc8ab5c969008dab8114e
Vitaly Bushlanov <144945733+snegov69@users.noreply.github.com>
2025-05-23 11:47:00 +0500

    init chrome and firefox

---
+++ lesson2/1_chromedriver.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.quit()
---
+++ lesson2/2_geckodriver.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+driver.quit()
